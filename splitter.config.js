519e4d8fcfcacac89daa9e266790babcd7725040
Florian Lorenzen <florian.lorenzen@posteo.de>
2020-08-19 23:21:45 +0200

    Config for fable-splitter

---
+++ splitter.config.js
@@ -0,0 +1,27 @@
+var path = require('path')
+
+function resolve(filePath) {
+  return path.resolve(__dirname, filePath)
+}
+
+var define = [];
+var idx = process.argv.indexOf("--define");
+
+if (idx > -1) {
+  define = [process.argv[idx + 1]];
+}
+
+console.log("Bundling for " + define + "...");
+
+module.exports = {
+  entry: resolve('src/JakBox.fsproj'),
+  outDir: resolve("out"),
+  babel: {
+    filename: "JakBox.js",
+    presets: ["module:metro-react-native-babel-preset"],
+    sourceMaps: true
+  },
+  fable: {
+    define: define
+  }
+};
